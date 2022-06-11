5df547f35a868a0c4719e6126ea152133a446469
Eric Canas <elcorreodeharu@gmail.com>
2022-06-11 23:16:45 +0200

    Setup updated again for pip

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pic2world',
-    version='0.1',
+    version='0.3',
     packages=find_packages('modules'),
     url='https://github.com/Eric-Canas/pic2world',
     license='MIT',
@@ -10,6 +10,8 @@ setup(
     author='Eric Canas',
     author_email='eric@ericcanas.com',
     description='P2W is a toolbox that implements several utilities for getting real-world information from pictures.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     install_requires=[
         'numpy',
     ],
